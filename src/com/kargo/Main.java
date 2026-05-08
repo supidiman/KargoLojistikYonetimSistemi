420e22128a2c55975be7f5f076df3494811bac7c
Yunus Emre Sarıbacak <85799852+YunusEmreSrb@users.noreply.github.com>
2026-05-08 23:35:08 +0300

    Add files via upload

--- src/com/kargo/Main.java
+++ src/com/kargo/Main.java
@@ -1,17 +1,18 @@
 package com.kargo;
 
 import com.kargo.ui.LoginFrame;
+import com.kargo.dao.DatabaseConnection;
 import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        // Arayüzün (UI) güvenli ve takılmadan çalışması için SwingUtilities kullanılır
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                // Program başladığında ekrana LoginFrame'i getir
-                new LoginFrame().setVisible(true);
-            }
+        // Veritabanı tablolarını ilk açılışta kontrol et/oluştur
+        DatabaseConnection.tablolariOlustur();
+        
+        SwingUtilities.invokeLater(() -> {
+            // Uygulamayı giriş ekranı ile başlat
+            LoginFrame girisEkrani = new LoginFrame();
+            girisEkrani.setVisible(true);
         });
     }
 }
