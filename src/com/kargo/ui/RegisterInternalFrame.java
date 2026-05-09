ff319e7ac2145534b7da10a4538ae8ba1f9ee54b
Yunus Emre Sarıbacak <85799852+YunusEmreSrb@users.noreply.github.com>
2026-05-09 10:15:58 +0300

    Update RegisterInternalFrame.java

--- src/com/kargo/ui/RegisterInternalFrame.java
+++ src/com/kargo/ui/RegisterInternalFrame.java
@@ -18,25 +18,25 @@ public class RegisterInternalFrame extends JInternalFrame
 
     public RegisterInternalFrame(MusteriService servis)
     {
-        super("yeni müsteri ekeleme ",false,true,false,true);
+        super("Yeni Müşteri Kaydı",false,true,false,true);
         this.servis=servis;
         this.setSize(350,350);
         this.setLayout(null);
         this.setLocation(50,80);
         
-        JLabel l1=new JLabel("adınız:"); l1.setBounds(30,30,100,25); this.add(l1);
+        JLabel l1=new JLabel("Ad Soyad:"); l1.setBounds(30,30,100,25); this.add(l1);
         ad=new JTextField(); ad.setBounds(130,30,150,25); this.add(ad);
         
-        JLabel l2=new JLabel("kimlik:"); l2.setBounds(30,70,100,25); this.add(l2);
+        JLabel l2=new JLabel("TC Kimlik No:"); l2.setBounds(30,70,100,25); this.add(l2);
         kimlik=new JTextField(); kimlik.setBounds(130,70,150,25); this.add(kimlik);
         
-        JLabel l3=new JLabel("tel:"); l3.setBounds(30, 110, 100, 25); this.add(l3);
+        JLabel l3=new JLabel("Telefon:"); l3.setBounds(30, 110, 100, 25); this.add(l3);
         tel=new JTextField(); tel.setBounds(130, 110, 150, 25); this.add(tel);
         
-        JLabel l4=new JLabel("adres:"); l4.setBounds(30, 150, 100, 25); this.add(l4);
+        JLabel l4=new JLabel("Adres:"); l4.setBounds(30, 150, 100, 25); this.add(l4);
         adres=new JTextField(); adres.setBounds(130, 150, 150, 25); this.add(adres);
         
-        JButton btn = new JButton("KAYDET"); btn.setBounds(130, 200, 150, 35); this.add(btn);
+        JButton btn = new JButton("Müşteriyi Kaydet"); btn.setBounds(130, 200, 150, 35); this.add(btn);
         
         btn.addActionListener(new ActionListener() 
         {
@@ -45,11 +45,11 @@ public class RegisterInternalFrame extends JInternalFrame
                 Musteri m = new Musteri(ad.getText(), kimlik.getText(), tel.getText(), adres.getText());
                 try {
                     servis.musteriKaydet(m);
-                    JOptionPane.showMessageDialog(null,"BASARIYLA KAYDEDILDI");
+                    JOptionPane.showMessageDialog(null,"Müşteri başarıyla sisteme kaydedildi.");
                     ad.setText(""); kimlik.setText(""); tel.setText(""); adres.setText("");
                 }
                 catch(Exception exe) {
-                    JOptionPane.showMessageDialog(null,"HATA: "+exe.getMessage());
+                    JOptionPane.showMessageDialog(null,"Kayıt başarısız: " + exe.getMessage());
                 }
             }
         });
